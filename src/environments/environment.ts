341dc6dd4fc4ed9a04e7dfd540efbf53157b009e
paulmcgonigle67 <s00210326@atu.ie>
2023-01-31 15:53:07 +0000

    environment Setuo

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: false
+};
+
+export const SpotifyConfiguration ={
+    clientId: 'c4cd5531ce004764a2ef3a24bcb776b4',
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    redirectUrl: 'http://localhost:4200/login/',
+    scopes:[
+        "user-read-currently-playing", 
+    "user-read-recently-played", 
+    "user-read-playback-state", 
+    "user-top-read", 
+    "user-modify-playback-state", 
+    "user-library-read", 
+    "playlist-read-private", 
+    "playlist-read-collaborative" 
+    ]
+
+}
